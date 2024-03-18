569884f30ec61ee75d9b305b89b02c884d043428
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-03-18 10:03:01 +0100

    feature : create feel section

--- src/app/components/home/home-container.tsx
+++ src/app/components/home/home-container.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Hero } from "./hero";
 import { HereForYou } from "./here-for-you";
 import { HealthDatas } from "./health-datas";
+import { FeelView } from "./feel";
 
 export default function HomeContainer() {
   return (
@@ -12,6 +13,7 @@ export default function HomeContainer() {
         <Hero />
         <HereForYou />
         <HealthDatas />
+        <FeelView />
       </Box>
     </PublicLayout>
   );
