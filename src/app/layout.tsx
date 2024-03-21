d989675f623c87eb967e5c097679902b9782495f
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-03-21 11:36:16 +0100

    feature : add providers to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./style.css";
+import { I18NProvider } from "@/core/i18n/i18n-provider";
+import { EnvProvider } from "@/core/providers/env-provider";
 
 export const metadata: Metadata = {
   title: "TakeCare",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <EnvProvider config={JSON.stringify(process.env)}>
+          <I18NProvider>{children}</I18NProvider>
+        </EnvProvider>
+      </body>
     </html>
   );
 }
