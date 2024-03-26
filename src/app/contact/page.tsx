4d827c1e07e9089f3d3560fdc51a14b76d66bb09
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-03-26 10:07:32 +0100

    feature : create files for new pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactScreen() {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+}
---
+++ src/app/policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PolicyScreen() {
+  return (
+    <div>
+      <h1>Policy</h1>
+    </div>
+  );
+}
