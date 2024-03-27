ad7317cde456b5e4f603df1f84d1553408b53a45
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-03-27 15:38:40 +0100

    feature : fix responsive from typo UI

--- src/core/ui/typography.tsx
+++ src/core/ui/typography.tsx
@@ -1,7 +1,6 @@
 import { Typography, styled } from "@mui/material";
 
 export const Paragraph = styled(Typography)({
-  fontSize: "1rem",
   color: "rgba(148, 148, 148, 1)",
   fontWeight: 300,
   lineHeight: "2",
