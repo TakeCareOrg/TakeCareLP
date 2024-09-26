8db5a3fa1b3ccbec96aa30e3993d70d35d45766e
N0ku <naku.pocket@gmail.com>
2024-09-26 17:25:05 +0200

    feat: add navbar and style

--- src/app/layouts/navbar.tsx
+++ src/app/layouts/navbar.tsx
@@ -1,19 +1,37 @@
-
 import { Lang } from "@/app/types";
 import { AvailableLocale } from "@/app/i18n/i18n";
 import Image from "next/image";
+import Link from "next/link";
+
+import "../styles/navbar.scss";
 
 export const Navbar: React.FC<{
   locale?: AvailableLocale;
   langs?: Lang[];
 }> = ({ locale, langs }) => {
   return (
-   <nav>
-    <div>
-      <Image src="/TakeCare_Logo.svg" alt="logo" width={100} height={100} />
-      <p></p>
-    </div>
-   </nav>
+    <nav>
+      <div className="nav-logo">
+        <Image
+          src="./images/TakeCare_Logo.svg"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+        <p>TakeCare AI</p>
+      </div>
+
+      <div className="nav-links">
+        <Link href="/about">About</Link>
+        <Link href="/goals">Our goals</Link>
+        <Link href="/roadmap">RoadMap</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+
+      <div className="nav-button">
+        <p>Try now</p>
+      </div>
+    </nav>
   );
 };
 
