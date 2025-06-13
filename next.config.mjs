64c226c546893d209c7c627d9ac20a2f61bc9803
Lucas Goncalves <87122664+RuKaSuu@users.noreply.github.com>
2025-06-13 14:27:03 +0200

    feat : metrics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        instrumentationHook: true,
+      },
+      
+      images: {
+        formats: ['image/webp', 'image/avif'],
+      },
+      
+      compress: true,
+      
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-DNS-Prefetch-Control',
+                value: 'on'
+              }
+            ],
+          },
+        ];
+      },
+};
+
+export default withBundleAnalyzer(nextConfig);
